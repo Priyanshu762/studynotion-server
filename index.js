095e8beed4bb7a2a9a19b788f1772f8f89703d30
Priyanshu Kumar <96052690+Priyanshu762@users.noreply.github.com>
2024-06-17 10:54:45 +0530

    Update Cors index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://studynotion-vecl.vercel.app/",
         credentials:true,
     })
 )
